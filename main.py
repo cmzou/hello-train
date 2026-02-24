1bb88940c12b2e5d444fd777526782d9ab73deea
unknown <39781351+cmzou@users.noreply.github.com>
2026-02-24 10:34:04 -0600

    add last update time

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import time
+import datetime
 import random
-from PIL import Image
+from PIL import Image, ImageDraw
 
 from inky.auto import auto
 
-inky = auto(ask_user=True, verbose=True)
+inky_display = auto(ask_user=True, verbose=True)
 
 image_paths = ["hudson.png", "hudson2.png", "hudson3.png"]
+
+def get_current_time() -> str:
+    current_time = datetime.datetime.today()
+    return current_time.strftime("%Y/%m/%d %H:%M:%S")
 
 def display_images():
     while True:
         image_path = random.choice(image_paths)
         image = Image.open("./images/" + image_path)
-        resizedimage = image.resize(inky.resolution)
-        inky.set_image(resizedimage)
-        inky.show()
+        resizedimage = image.resize(inky_display.resolution)
+
+        draw = ImageDraw.Draw(resizedimage)
+        draw.text((0, 0), f"Last Updated: {get_current_time()}", inky_display.BLACK)
+
+        inky_display.set_image(resizedimage)
+        inky_display.show()
         time.sleep(5*60)
 
 def main():
