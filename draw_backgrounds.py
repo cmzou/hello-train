3e476eee167c3c4782159ead3545b8d6116be194
unknown <39781351+cmzou@users.noreply.github.com>
2026-03-01 08:22:20 -0600

    initial text alignment code

--- draw_backgrounds.py
+++ draw_backgrounds.py
@@ -90,10 +90,31 @@ def create_arrivals_background(arrivals_data: pd.DataFrame, image: Image) -> Ima
 Given the coordinates of a box, align text within the box.
 
 Params:
+    xy: box coordinates of the form [(x0, y0), (x1, y1)]
     v_align: vertical alignment by relative position within the box. 0.5 corresponds to center
+    v_align: horizontal alignment by relative position within the box. 0.5 corresponds to center
 """
-def add_text_to_box(box_x1: int, box_y1: int, box_x2: int, box_y2: int, v_align=0.5, h_align=0.5, align="left") -> None:
+def add_text_to_box(xy: list[tuple], v_align: float=0.5, h_align: float=0.5, align: str="left") -> None:
     pass
+
+
+"""
+Given desired coordinates and alignment, return aligned coordinates
+"""
+def align_text(xy: list[tuple], text: str, font: ImageFont.FreeTypeFont, h_align: float=0.5, v_align: float=0.5, align: str="left") -> list[tuple]:
+    x, y = xy
+    box_left, box_top, box_right, box_bottom = font.getbbox(text)
+
+    text_height = box_bottom - box_top
+
+    print(text_height)
+
+    y_offset = text_height * v_align
+
+    if align == "left":
+        return (x, y - y_offset)
+    if align == "right":
+        return (x - box_right, y - y_offset)
 
 """
 Adds coordinates at key parts of the image for debugging purposes.
@@ -101,11 +122,20 @@ Adds coordinates at key parts of the image for debugging purposes.
 def add_grid_coord(image: Image, color=inky_display.WHITE) -> Image:
     draw = ImageDraw.Draw(image)
 
-    draw.text((0, 50), "(0, 50)", color, font=fnt_small)
-    draw.text((50, 50), "(50, 50)", color, font=fnt_small)
-    draw.text((0, 200), "(0, 200)", color, font=fnt_small)
-    draw.text((0, 250), "(0, 250)", color, font=fnt_small)
-    draw.text((0, 300), "(0, 300)", color, font=fnt_small)
+    # draw.text((0, 50), "(0, 50)", color, font=fnt_small)
+    # draw.text((50, 50), "(50, 50)", color, font=fnt_small)
+    # draw.text((0, 200), "(0, 200)", color, font=fnt_small)
+    # draw.text((0, 250), "(0, 250)", color, font=fnt_small)
+    # draw.text((0, 300), "(0, 300)", color, font=fnt_small)
+
+    draw.line(((0, 50), (inky_display.width, 50)))
+    draw.line(((50, 0), (50, inky_display.height)))
+
+    new_align = align_text((50,50), "Hello", fnt_large, align="right")
+    print(new_align)
+
+    # draw.text((50,50), "Hello", color="red", font=fnt_small)
+    draw.text(new_align, "Hello", color="blue", font=fnt_large)
 
     return image
 
@@ -160,6 +190,7 @@ if __name__ == "__main__":
         "isDly": [0, 0, 0, 0],
         "idFlt": [0, 0, 0, 0]
     })
-    image = create_arrivals_background(arrivals_data4, image)
+    # image = create_arrivals_background(arrivals_data4, image)
+    image = add_grid_coord(image)
     save_image(image, os.path.join(ui_dir, "./cta_ui.png"))
     image.show()
