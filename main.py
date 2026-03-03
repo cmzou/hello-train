060677d9e54c5d721ad1d0102f63d2421b33ad1e
unknown <39781351+cmzou@users.noreply.github.com>
2026-03-02 19:15:38 -0600

    fix missing arg

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     try:
         state_machine.main()
     except Exception as e:
-        image_cycler.displays["error"].display_current_image()
+        image_cycler.displays["error"].display_current_image(state_machine.inky_display)
         print("Error detected:", e)
