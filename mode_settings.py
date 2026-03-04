d38c6db48cb2ad0ff1f391870f8fc0e51630d318
cmzou <39781351+cmzou@users.noreply.github.com>
2026-03-04 16:54:14 -0600

    set up for scheduled modes

--- mode_settings.py
+++ mode_settings.py
@@ -9,11 +9,16 @@ display_bus_stops = ["Ashland & Van Buren"] # which bus stops to display
 
 cta_refresh_seconds = 60 * 5 # not recommended < 3 minutes due to refresh speed and display lifespan
 
+min_arrival_to_omit = 5 # minutes until arrival to omit if less than; useful to set as the minimum time to get to station/stop
+
+enable_scheduled_display = True # whether to automatically switch to this mode during certain time periods
+scheduled_intervals = [("7:00 AM", "7:30 AM")] # list[tuple[str, str]] of times to auto switch to; inclusion: [start, end)
+
 #### Cats mode ###
 
 image_dir = "./images" # directory of images to show
 enable_shuffle = True # whether to shuffle images on display
 
-enable_scheduled_shuffle = True
+enable_scheduled_shuffle = True # whether to refresh the image at the same time each day
 scheduled_refresh_time = "9:00 PM" # when to cycle image each day; ignored if enable_scheduled_shuffle is False
 shuffle_seconds = 60 * 5 # interval between image cycles; ignored if enable_scheduled_shuffle is True
